72f6406ae726b43c30b36e0a60a59314c420018b
xzjqx <xzjqx@outlook.com>
2018-07-25 12:02:13 +0800

    Update "Main.java", in order to print all tables.

--- src/main/java/cn/edu/tju/tiei/logistics/main/Main.java
+++ src/main/java/cn/edu/tju/tiei/logistics/main/Main.java
@@ -9,8 +9,16 @@ import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
+import cn.edu.tju.tiei.logistics.dao.BidMapper;
+import cn.edu.tju.tiei.logistics.dao.CarrierMapper;
+import cn.edu.tju.tiei.logistics.dao.ShipperMapper;
 import cn.edu.tju.tiei.logistics.dao.UsrMapper;
+import cn.edu.tju.tiei.logistics.dao.WaybillMapper;
+import cn.edu.tju.tiei.logistics.model.Bid;
+import cn.edu.tju.tiei.logistics.model.Carrier;
+import cn.edu.tju.tiei.logistics.model.Shipper;
 import cn.edu.tju.tiei.logistics.model.Usr;
+import cn.edu.tju.tiei.logistics.model.Waybill;
 
 public class Main {
 
@@ -23,11 +31,40 @@ public class Main {
 			SqlSessionFactory sqlSessionFactory = 
 					new SqlSessionFactoryBuilder().build(inputStream);
 			SqlSession sqlSession = sqlSessionFactory.openSession();
-			UsrMapper mapper = sqlSession.getMapper(UsrMapper.class);
-			List<Usr> usrs = mapper.selectByExample(null);
+			
+			BidMapper bidMapper = sqlSession.getMapper(BidMapper.class);
+			List<Bid> bids = bidMapper.selectByExample(null);
+			
+			for (Bid bid : bids) {
+				System.out.println(bid);
+			}
+			
+			CarrierMapper carrierMapper = sqlSession.getMapper(CarrierMapper.class);
+			List<Carrier> carriers = carrierMapper.selectByExample(null);
+			
+			for (Carrier carrier : carriers) {
+				System.out.println(carrier);
+			}
+			
+			ShipperMapper shipperMapper = sqlSession.getMapper(ShipperMapper.class);
+			List<Shipper> shippers = shipperMapper.selectByExample(null);
+			
+			for (Shipper shipper : shippers) {
+				System.out.println(shipper);
+			}
+			
+			UsrMapper usrMapper = sqlSession.getMapper(UsrMapper.class);
+			List<Usr> usrs = usrMapper.selectByExample(null);
 			
 			for (Usr usr : usrs) {
 				System.out.println(usr);
+			}
+			
+			WaybillMapper waybillMapper = sqlSession.getMapper(WaybillMapper.class);
+			List<Waybill> waybills = waybillMapper.selectByExample(null);
+			
+			for (Waybill waybill : waybills) {
+				System.out.println(waybill);
 			}
 			
 		} catch (IOException e) {
